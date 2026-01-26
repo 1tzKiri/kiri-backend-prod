01426fc6ef68079101159f97848c9608fc2288a8
1tzKiri <d4k8brgksm@privaterelay.appleid.com>
2026-01-26 21:05:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  res.send("OK FROM RAILWAY");
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Kiri backend running on port " + PORT);
+  console.log("Listening on", PORT);
 });
-
