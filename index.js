cc6412bb792aee7c2893385190107def5bd5a6fa
1tzKiri <d4k8brgksm@privaterelay.appleid.com>
2026-05-25 12:36:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -996,7 +996,7 @@ app.post("/create-checkout", async (req, res) => {
 app.get("/make-admin", async (req, res) => {
 
   await pool.query(
-    "UPDATE users SET role = 'admin' WHERE email = 'YOUR_EMAIL'"
+    "UPDATE users SET role = 'admin' WHERE email = 'ygreek2012@gmail.com'"
   );
 
   res.send("done");
